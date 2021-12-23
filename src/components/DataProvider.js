691c5ba5cc462e47997a2f17ad0bdf6f84276eec
Duy-V <70538526+Duy-V@users.noreply.github.com>
2021-12-24 00:34:39 +0700

    TODO APP

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -1,21 +1,59 @@
-import React, {useState, useEffect, createContext} from 'react';
+import React, { useState, useEffect, createContext } from "react";
 export const DataContext = createContext();
-export const  DataProvider = (props) => {
-    const [todos, setTodos]= useState([])
-    /*effect dung để khi reload lại trang thì  không đổi  list item */
-    useEffect(()=> {
-      const todoStore=JSON.parse(localStorage.getItem('todoStore'))
-       if(todoStore) setTodos(todoStore);
-  },[])
-    useEffect(()=>{
-        localStorage.setItem('todoStore', JSON.stringify(todos))
-    },[todos])
-    
-    
+export const DataProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+  /*effect dung để khi reload lại trang thì  không đổi  list item */
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+  //fetch todos
+  const fetchTodos = async () => {
+    const response = await fetch(`/todos?_sort=id&_order=desc`);
+    const data = await response.json();
+    setTodos(data);
+  };
+  //add todo
+  const addTodos = async (newTodos) => {
+    const response = await fetch("/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTodos),
+    });
+    const data = await response.json();
+    setTodos([data, ...todos]);
+    console.log(data);
+  };
+  //delete todos
+  const deleteTodos = async (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/todos/${id}`, { method: "DELETE" });
+      setTodos(todos.filter((item) => item.id !== id));
+    }
+  };
+  const updateTodos = async (id, upItem) => {
+    const response = await fetch(`/todos/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(upItem),
+    });
+    const data = await response.json();
+    setTodos(
+      todos.map((item) => (item.id === id ? { ...item, ...data } : item))
+    );
+  };
+  useEffect(() => {
+    const todoStore = JSON.parse(localStorage.getItem("todoStore"));
+    if (todoStore) setTodos(todoStore);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todoStore", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <DataContext.Provider value={[todos, setTodos]}>
-      {props.children}
+      {children}
     </DataContext.Provider>
-  )
-}
-
+  );
+};
